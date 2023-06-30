461d3ae4d9ff629fd54cff36a3e2d31916e1e717
Akshat-Shah-22 <akshatshah535@gmail.com>
2023-06-30 11:21:10 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Typescript MERN ECOMM
+
+1. Create Typescript React App By Vite
+2. Create Git Repository
---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <header>TS EKART</header>
+      <main></main>
+      <footer>All rights reserved.</footer>
+    </div>
+    </>
+  )
+}
+
+export default App
