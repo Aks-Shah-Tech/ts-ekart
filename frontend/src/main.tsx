d7b1ea6260f80e524882e8f8273b1fa5cd456839
Akshat-Shah-22 <akshatshah535@gmail.com>
2023-07-04 17:02:09 +0530

    Signup page created

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -18,6 +18,7 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { StoreProvider } from './Store.tsx';
 import CartPage from './pages/CartPage.tsx';
 import SigninPage from './pages/SigninPage.tsx';
+import SignupPage from './pages/SignupPage.tsx';
 
 
 const router = createBrowserRouter(
@@ -27,6 +28,7 @@ const router = createBrowserRouter(
       <Route path="/product/:slug" element={<ProductPage />} />
       <Route path="cart" element={<CartPage />} />
       <Route path="signin" element={<SigninPage />} />
+      <Route path="signup" element={<SignupPage />} />
       {/* <Route path="dashboard" element={<Dashboard />} /> */}
       {/* ... etc. */}
     </Route>
